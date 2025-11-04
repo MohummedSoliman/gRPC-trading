b253adcf53fd16bc625795de7979ad1b5be2c888
Mohamed Ibranim <mosuliman92@gmail.com>
2025-11-04 09:09:38 +0200

    create repositories for entity classes

--- user-service/src/main/java/com/mohamed/user_service/repository/PortfolioItemRepository.java
+++ user-service/src/main/java/com/mohamed/user_service/repository/PortfolioItemRepository.java
@@ -1,4 +1,17 @@
 package com.mohamed.user_service.repository;
 
-public interface PortfolioItemRepository {
+import com.mohamed.common.Ticker;
+import com.mohamed.user_service.entity.PortfolioItem;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PortfolioItemRepository extends CrudRepository<PortfolioItem, Integer> {
+
+    List<PortfolioItem> findAllByUserId(Integer userId);
+
+    Optional<PortfolioItem> findByUserIdAndTicker(Integer userId, Ticker ticker);
 }
--- user-service/src/main/java/com/mohamed/user_service/repository/UserRepository.java
+++ user-service/src/main/java/com/mohamed/user_service/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.mohamed.user_service.repository;
 
-public interface UserRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Integer> {
 }
