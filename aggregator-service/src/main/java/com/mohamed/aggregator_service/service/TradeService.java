8ef5b5d6a9bb9411a5378ed9dd37527d3c6b63a2
Mohamed Ibranim <mosuliman92@gmail.com>
2025-11-10 14:09:05 +0200

    create service layer for client and stock

--- aggregator-service/src/main/java/com/mohamed/aggregator_service/service/TradeService.java
+++ aggregator-service/src/main/java/com/mohamed/aggregator_service/service/TradeService.java
@@ -1,4 +1,32 @@
 package com.mohamed.aggregator_service.service;
 
+import com.mohamed.stock.StockPriceRequest;
+import com.mohamed.stock.StockServiceGrpc;
+import com.mohamed.user.StockTradeRequest;
+import com.mohamed.user.StockTradeResponse;
+import com.mohamed.user.UserServiceGrpc;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TradeService {
+
+    @GrpcClient("user-service")
+    private UserServiceGrpc.UserServiceBlockingStub userClient;
+
+    @GrpcClient("stock-service")
+    private StockServiceGrpc.StockServiceBlockingStub stockClient;
+
+    public StockTradeResponse trade(StockTradeRequest request) {
+        var priceRequest = StockPriceRequest.newBuilder()
+                .setTicker(request.getTicker())
+                .build();
+
+        var priceResponse = this.stockClient.getStockPrice(priceRequest);
+        var tradeRequest = request.toBuilder()
+                .setPrice(priceResponse.getPrice())
+                .build();
+
+        return this.userClient.tradeStock(tradeRequest);
+    }
 }
--- aggregator-service/src/main/java/com/mohamed/aggregator_service/service/UserService.java
+++ aggregator-service/src/main/java/com/mohamed/aggregator_service/service/UserService.java
@@ -1,4 +1,22 @@
 package com.mohamed.aggregator_service.service;
 
+import com.mohamed.user.UserInformation;
+import com.mohamed.user.UserInformationRequest;
+import com.mohamed.user.UserServiceGrpc;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @GrpcClient("user-service")
+    private UserServiceGrpc.UserServiceBlockingStub userClient;
+
+    public UserInformation getUserInformation(int userId) {
+        var request = UserInformationRequest.newBuilder()
+                .setUserId(userId)
+                .build();
+
+        return this.userClient.getUserInformation(request);
+    }
 }
